ff9449131fc136c8eab27d8a98139cc04a707a59
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-06-17 22:01:40 +0800

    Add skills and tags to homepage

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,10 @@ export const SITE: Site = {
   TITLE: "Andrian Lloyd Maagma",
   DESCRIPTION: "This is Andrian Lloyd Maagma's personal website.",
   EMAIL: "maagmaandrian@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_SKILLS_ON_HOMEPAGE: 10,
+  NUM_TAGS_ON_HOMEPAGE: 10,
 };
 
 export const HOME: Metadata = {
--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,10 @@ export type Site = {
   TITLE: string;
   DESCRIPTION: string;
   EMAIL: string;
-  NUM_POSTS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
+  NUM_POSTS_ON_HOMEPAGE: number;
+  NUM_SKILLS_ON_HOMEPAGE: number;
+  NUM_TAGS_ON_HOMEPAGE: number;
 };
 
 export type Metadata = {
