b7b24b08510a73844ee24d94a67b5e36e224c213
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-06-19 13:10:17 +0800

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [sitemap(), mdx(), pagefind(), icon()],
   vite: {
     plugins: [tailwindcss()],
     server: {
