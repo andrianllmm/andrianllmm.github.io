6b15738a124b7645473f7c2cb38f3ef8cb52184c
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-02-07 15:19:21 +0800

    Pin specific projects

--- src/consts.ts
+++ src/consts.ts
@@ -12,6 +12,13 @@ export const SITE: Site = {
   NUM_EDUCATION_ON_HOMEPAGE: 5,
 };
 
+export const PINNED_PROJECTS = [
+  "manobela",
+  "renux",
+  "uniglyphs",
+  "tidal-island",
+];
+
 export const HOME: Metadata = {
   TITLE: "Home",
   DESCRIPTION: "This is Andrian Lloyd Maagma's personal website.",
